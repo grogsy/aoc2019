05c82d24ba52b0e2314924f0e788d2afe40d06bb
Brian <a.gray.cloud@gmail.com>
2019-12-04 01:59:31 -0400

    part 2 solution

--- 1.py
+++ 1.py
@@ -4,7 +4,17 @@ from functools import reduce
 def calc(num):
     return floor(num / 3) - 2
 
+def get_n_fuel(num):
+    out = calc(num)
+    next_num = calc(out)
+    while next_num > 0:
+        out += next_num
+        next_num = calc(next_num)
+
+    return out
+
 data = map(int, open('1.txt').readlines())
-print(sum(calc(num) for num in data))
+# print(sum(calc(num) for num in data))
+print(sum(get_n_fuel(num) for num in data))
 
 # print(reduce(lambda x, y: x + (floor(y / 3) - 2), map(int, open('1.txt').readlines()), 0))
