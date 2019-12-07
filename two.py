3159e6aa26a07172300ae39017cf0aa15b06e3bd
Brian <a.gray.cloud@gmail.com>
2019-12-07 12:50:04 -0400

    solution part 2

--- two.py
+++ two.py
@@ -8,6 +8,10 @@ class Computer:
         2, # mul(a, b)
         3, # mov(a)
         4, # print(a)
+        5, # jt(a, b)
+        6, # jf(a, b)
+        7, # lt(a, b)
+        8, # eq(a, b)
         99 # exit
     }
     param_modes = {
@@ -24,9 +28,8 @@ class Computer:
         data[1] = noun if noun else self.instructions[1]
         data[2] = verb if verb else self.instructions[2]
 
-        i = 0
-        while i < len(data): 
-            op = data[i]
+        while self.counter < len(data): 
+            op = data[self.counter]
             params = None
             if op > 9 and op != 99:
                 long = str(op)
@@ -34,31 +37,44 @@ class Computer:
                 params = long[:-2]
 
             if op not in self.op_codes:
-                i += 4
+                self.counter += 4
                 continue
             elif op == 99:
                 break
-
-            x, y, pos = data[i + 1], data[i + 2], data[i + 3]
-
-            if op == 1:
-                x = x if params is not None and params[-1] == '1' else data[x]
-                y = y if params is not None and len(params) > 1 and params[-2] == '1' else data[y]
-                i += 4
-                val = x + y
-            elif op == 2:
-                x = x if params is not None and params[-1] == '1' else data[x]
-                y = y if params is not None and len(params) > 1 and params[-2] == '1' else data[y]
-                i += 4
-                val = x * y
             elif op == 3:
                 val = int(input("Opcode 3, Diagnostic Input: "))
-                i += 2
-                pos = data[i + 1]
-            elif op == 4:
-                print("Diagnostic, value at data[{}]: {}".format(x, data[x]))
-                i +=2
+                self.counter += 2
+                pos = data[self.counter + 1]
+                data[pos] = val
                 continue
+            elif op == 4:
+                x = data[self.counter + 1]
+                print("Diagnostic, value at data[{}]: {}".format(x, data[x]))
+                self.counter +=2
+                continue
+            
+            x, y, pos = data[self.counter + 1], data[self.counter + 2], data[self.counter + 3]
+            x = x if params is not None and params[-1] == '1' else data[x]
+            y = y if params is not None and len(params) > 1 and params[-2] == '1' else data[y]
+
+            if op == 1:
+                self.counter += 4
+                val = x + y
+            elif op == 2:
+                self.counter += 4
+                val = x * y
+            elif op == 5:
+                self.counter = y if x else self.counter + 3
+                continue
+            elif op == 6:
+                self.counter = y if x == 0 else self.counter + 3
+                continue
+            elif op == 7:
+                val = 1 if x < y else 0
+                self.counter += 4
+            elif op == 8:
+                val = 1 if x == y else 0
+                self.counter += 4
 
             data[pos] = val
 
