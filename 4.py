4e1443e82e8019532a5fbdf69e8f2536df168112
Brian <a.gray.cloud@gmail.com>
2019-12-06 23:42:59 -0400

    refactor solution

--- 4.py
+++ 4.py
@@ -1,28 +1,27 @@
 check = range(153517, 630396)
 
 def has_double(num) -> int:
-    num = list(map(int, str(num)))
     return any(num[i] == num[i + 1] for i in range(len(num) - 1))
 
 def increasing(num) -> int:
-    num = list(map(int, str(num)))
     return all(num[i] <= num[i + 1] for i in range(len(num) - 1))
 
-def part_two(num):
-    num = list(map(int, str(num)))
+def only_two(num):
     memo = {d: 0 for d in num}
     for d in num:
         memo[d] += 1
     return any(memo.get(k) == 2 for k in memo)
 
+def possible_pass(num):
+    digits = list(map(int, str(num)))
+
+    return increasing(digits) and has_double(digits) and only_two(digits)
 
 count = 0
 
 for i in check:
-    if increasing(i):
-        if has_double(i):
-            if part_two(i):
-                count += 1
+    if possible_pass(i):
+        count += 1
 
 
 print(count)
