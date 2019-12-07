3ac83a5ccd1666963cdce381096bef9267a1094e
Brian <a.gray.cloud@gmail.com>
2019-12-06 20:28:54 -0400

    solution part 2

--- 4.py
+++ 4.py
@@ -8,5 +8,21 @@ def increasing(num) -> int:
     num = list(map(int, str(num)))
     return all(num[i] <= num[i + 1] for i in range(len(num) - 1))
 
+def part_two(num):
+    num = list(map(int, str(num)))
+    memo = {d: 0 for d in num}
+    for d in num:
+        memo[d] += 1
+    return any(memo.get(k) == 2 for k in memo)
 
-print(len([num for num in check if has_double(num) and increasing(num)]))
+
+count = 0
+
+for i in check:
+    if increasing(i):
+        if has_double(i):
+            if part_two(i):
+                count += 1
+
+
+print(count)
