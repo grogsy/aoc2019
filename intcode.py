3879b0e43671f76bf1011943b74fa95022ffb0b0
Brian <a.gray.cloud@gmail.com>
2019-12-16 18:11:02 -0400

    refactor intcode to support day 9 features

--- intcode.py
+++ intcode.py
@@ -2,7 +2,6 @@
 class InvalidOpCode(BaseException):
     pass
 
-
 class Computer:
     op_codes = {
         1,  # add(a, b)
@@ -13,12 +12,23 @@ class Computer:
         6,  # jne(a, b) jump not equal to 0
         7,  # lt(a, b)
         8,  # eq(a, b)
+        9,  # move relative base
         99  # exit
     }
 
-    def __init__(self, instructions):
+    param_modes = {
+        0: 'position_mode',
+        1: 'immediate_mode',
+        2: 'relative_mode'
+    }
+
+    def __init__(self, instructions, extend = False):
         self.instructions = instructions
+        if extend:
+            # make extends memory an optional feature
+            self.instructions += ([0] * 2048)
         self.counter = 0
+        self.relative_base = 0
 
         # subclasses will handle pause mechanism somehow
         self.paused = False
@@ -27,30 +37,30 @@ class Computer:
         self.working_set = self.instructions[:] 
 
         while self.counter < len(self.working_set):
+            # print(self.working_set[self.counter: self.counter+4])
             op = self.working_set[self.counter]
-            params = {}
-            if op > 9 and op != 99:
-                param_op = str(op)
-                op, params = self.parse_parameter_mode(param_op)
+            if op == 99:
+                self.handle_close()
+                break
+            
+            op, params = self.parse_operation(str(op))
+
+            x = self.working_set[self.counter + 1]
+            x = self.parse_parameter_mode(x, params.get('x'))
 
             if op not in Computer.op_codes:
                 self.handle_unrecognized_opcode(op)
                 continue
-            elif op == 99:
-                self.handle_close()
-                break
-            elif op in (3, 4):
-                self.handle_io(op)
+            elif op in (3, 4, 9):
+                self.handle_single_param(op, x)
                 if self.paused:
                     break
                 continue
 
-            x, y, pos = self.working_set[self.counter + 1], self.working_set[self.counter + 2], self.working_set[self.counter + 3]
+            y = self.working_set[self.counter + 2]
+            y = self.parse_parameter_mode(y, params.get('y'))
 
-            if not params.get('x_immediate_mode'):
-                x = self.working_set[x]
-            if not params.get('y_immediate_mode'):
-                y = self.working_set[y]
+            pos = self.working_set[self.counter + 3]
 
             if op in (5, 6):
                 self.jcmp(op, x, y)
@@ -59,13 +69,30 @@ class Computer:
         
         self.counter = 0
 
-    def parse_parameter_mode(self, operation):
+    def parse_parameter_mode(self, value, param):
+        if param == 'position_mode':
+            value = self.working_set[value]
+        elif param == 'relative_mode':
+            value = self.working_set[self.relative_base + value]
+        elif param != 'immediate_mode':
+            raise ValueError('unrecognized param: {}'.format(param))
+
+        return value
+
+    def parse_operation(self, operation):
         op = int(operation[-2:])
-        param_codes = operation[:-2]
+        x = 0
+        y = 0
+
+        if len(operation) > 2:
+            param_codes = operation[:-2]
+            x = int(param_codes[-1])
+            if len(param_codes) > 1:
+                y = int(param_codes[-2])
 
         params = {
-            'x_immediate_mode': True if param_codes[-1] == '1' else False,
-            'y_immediate_mode': True if (len(param_codes) > 1 and param_codes[-2] == '1') else False,
+            'x': Computer.param_modes.get(x, 'position_mode'),
+            'y': Computer.param_modes.get(y, 'position_mode')
         }
 
         return op, params
@@ -76,16 +103,25 @@ class Computer:
     def handle_close(self):
         pass
 
-    def handle_io(self, operation):
-        if operation == 3:
-            self.handle_input()
-        if operation == 4:
-            self.handle_output()
+    def handle_single_param(self, operation, value):
+        if operation == 9:
+            self.relative_base += value
+            self.counter += 2
+        elif operation in (3, 4):
+            self.handle_io(operation, value)
+        else:
+            print("unknown op {}".format(operation))
 
-    def handle_input(self):
+    def handle_io(self, operation, value):
+        if operation == 3:
+            self.handle_input(value)
+        if operation == 4:
+            self.handle_output(value)
+
+    def handle_input(self, value):
         raise NotImplementedError
 
-    def handle_output(self):
+    def handle_output(self, value):
         raise NotImplementedError
 
 
@@ -115,16 +151,17 @@ class DiagnosticTest(Computer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def handle_input(self):
+    def handle_input(self, value):
         val = int(input("Opcode 3, Diagnostic Input: "))
 
-        pos = self.working_set[self.counter + 1]
-        self.working_set[pos] = val
+        # pos = self.working_set[self.counter + 1]
+        self.working_set[value] = val
         self.counter += 2
     
-    def handle_output(self):
-        address = self.working_set[self.counter + 1]
-        print("Diagnostic Test, value at address {}: {}".format(address, self.working_set[address]))
+    def handle_output(self, value):
+        # address = self.working_set[self.counter + 1]
+        # print("Diagnostic Test, value at address {}: {}".format(address, self.working_set[address]))
+        print("Diagnostic Test Value: {}".format(value))
         self.counter += 2
 
 class AmplifierController(Computer):
@@ -146,7 +183,7 @@ class AmplifierController(Computer):
     def handle_close(self):
         self.halt = True
 
-    def handle_input(self):
+    def handle_input(self, _):
         if self.input_tracker == 0:
             # get input from phase_setting
             val = self.phase_setting
@@ -159,9 +196,10 @@ class AmplifierController(Computer):
         self.working_set[address] = val
         self.counter += 2
 
-    def handle_output(self):
-        address = self.working_set[self.counter + 1]
-        self.input_signal = self.working_set[address]
+    def handle_output(self, value):
+        # address = self.working_set[self.counter + 1]
+        # self.input_signal = self.working_set[address]
+        self.input_signal = value
         self.counter += 2
         if not self.silent:
             print(self.input_signal)
